899df000704751b125809173a0e73638cca4fd20
jaime-martinez <jaime.martinez@laureate.net>
2021-02-24 14:07:20 -0600

    Module 9

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,4 +1,4 @@
-import path from 'path';
+import path, { resolve } from 'path';
 import fetch from 'isomorphic-fetch';
 
 async function turnPizzasIntoPages({ graphql, actions }) {
@@ -57,9 +57,12 @@ async function fetchBeersAndTurnIntoNodes({
   createNodeId,
   createContentDigest,
 }) {
-  const res = await fetch('https://sampleapis.com/beers/api/ale');
+  // 1. Fetch a  list of beers
+  const res = await fetch('https://api.sampleapis.com/beers/stouts');
   const beers = await res.json();
+  // 2. Loop over each one
   for (const beer of beers) {
+    // create a node for each beer
     const nodeMeta = {
       id: createNodeId(`beer-${beer.name}`),
       parent: null,
@@ -70,11 +73,56 @@ async function fetchBeersAndTurnIntoNodes({
         contentDigest: createContentDigest(beer),
       },
     };
+    // 3. Create a node for that beer
     actions.createNode({
       ...beer,
       ...nodeMeta,
     });
   }
+}
+
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  data.slicemasters.nodes.forEach((slicemaster) => {
+    actions.createPage({
+      component: resolve('./src/templates/Slicemaster.js'),
+      path: `/slicemaster/${slicemaster.slug.current}`,
+      context: {
+        name: slicemaster.person,
+        slug: slicemaster.slug.current,
+      },
+    });
+  });
+
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+  console.log(pageCount);
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    console.log('creating page');
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
 }
 
 export async function sourceNodes(params) {
@@ -85,5 +133,6 @@ export async function createPages(params) {
   await Promise.all([
     await turnToppingsIntoPages(params),
     await turnPizzasIntoPages(params),
+    turnSlicemastersIntoPages(params),
   ]);
 }
--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,88 @@
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 import React from 'react';
+import styled from 'styled-components';
+import Pagination from '../components/Pagination';
 
-export default function SlicemastersPage() {
+const SliceMasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SlicemasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: --2rem;
+    position: relative;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+
+export default function SlicemastersPage({ data, pageContext }) {
+  const slicemasters = data.slicemasters.nodes;
   return (
     <>
-      <p>Hey! I'm the Slicemasters page</p>
+      <Pagination
+        pageSize={parseInt(process.env.GATSBY_PAGE_SIZE)}
+        totalCount={data.slicemasters.totalCount}
+        currentPage={pageContext.currentPage || 1}
+        skip={pageContext.skip}
+        base="/slicemasters"
+      />
+      <SliceMasterGrid>
+        {slicemasters.map((person) => (
+          <SlicemasterStyles key={person.id}>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </SlicemasterStyles>
+        ))}
+      </SliceMasterGrid>
     </>
   );
 }
+
+export const query = graphql`
+  query($skip: Int = 0, $pageSize: Int = 4) {
+    slicemasters: allSanityPerson(limit: $pageSize, skip: $skip) {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
