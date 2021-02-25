98fe810fe2e3a7c9c3f08fae2ed7953382f05dd4
jaime-martinez <jaime.martinez@laureate.net>
2021-02-25 15:27:30 -0600

    Finish Module 9

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import { graphql } from 'gatsby';
+import SEO from '../components/SEO';
 
 const PizzaGrid = styled.div`
   display: grid;
@@ -10,16 +12,40 @@ const PizzaGrid = styled.div`
 
 export default function SinglePizzaPage({ data: { pizza } }) {
   return (
-    <PizzaGrid>
-      <Img fluid={pizza.image.asset.fluid} />
-      <div>
-        <h2 className="mark">{pizza.name}</h2>
-        <ul>
-          {pizza.toppings.map((topping) => (
-            <li key={topping.id}> {topping.name} </li>
-          ))}
-        </ul>
-      </div>
-    </PizzaGrid>
+    <>
+      <SEO title={pizza.name} image={pizza.image?.asset?.fluid?.src} />
+      <PizzaGrid>
+        <Img fluid={pizza.image.asset.fluid} />
+        <div>
+          <h2 className="mark">{pizza.name}</h2>
+          <ul>
+            {pizza.toppings.map((topping) => (
+              <li key={topping.id}> {topping.name} </li>
+            ))}
+          </ul>
+        </div>
+      </PizzaGrid>
+    </>
   );
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    pizza: sanityPizza(slug: { current: { eq: $slug } }) {
+      name
+      id
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        name
+        id
+        vegetarian
+      }
+    }
+  }
+`;
--- gatsby/src/templates/Slicemaster.js
+++ gatsby/src/templates/Slicemaster.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import { graphql } from 'gatsby';
+import SEO from '../components/SEO';
 
 export default function SlicemasterPage({ data: { person } }) {
   return (
-    <div className="center">
-      <Img fluid={person.image.asset.fluid} />
-      <h2>
-        <span className="mark">{person.name}</span>
-      </h2>
-      <p>{person.description}</p>
-    </div>
+    <>
+      <SEO title={`${person.name}`} />
+      <div className="center">
+        <Img fluid={person.image.asset.fluid} />
+        <h2>
+          <span className="mark">{person.name}</span>
+        </h2>
+        <p>{person.description}</p>
+      </div>
+    </>
   );
 }
 
